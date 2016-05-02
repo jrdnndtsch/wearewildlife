c8fb624079dd6a61dec98910e5b58dded3aef2fa
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-05-02 11:20:52 -0400

    add devise user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502150644) do
+ActiveRecord::Schema.define(version: 20160502151654) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -64,5 +64,33 @@ ActiveRecord::Schema.define(version: 20160502150644) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "sub_menus", force: :cascade do |t|
+    t.string   "title"
+    t.string   "link"
+    t.integer  "main_menu_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "sub_menus", ["main_menu_id"], name: "index_sub_menus_on_main_menu_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
