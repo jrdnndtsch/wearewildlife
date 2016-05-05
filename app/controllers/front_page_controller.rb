5940e859f8366dd6d7e0616c23b053ff69c05f0a
deartrudence <trudy.macnabb@gmail.com>
2016-05-05 16:05:23 -0400

    instagram api

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -1,5 +1,7 @@
 class FrontPageController < ApplicationController
 	skip_before_action :authenticate_user!
   def show
+  	@instagram = Instagram.user_recent_media(@deartrudence)
+  	# raise "hell"
   end
 end
