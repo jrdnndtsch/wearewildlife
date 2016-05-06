cf7fe37e7cfa65766a2fefa1e59901787e06a37b
deartrudence <trudy.macnabb@gmail.com>
2016-05-06 11:30:14 -0400

    changes

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -1,7 +1,7 @@
 class FrontPageController < ApplicationController
 	skip_before_action :authenticate_user!
   def show
-  	@instagram = Instagram.user_recent_media(@deartrudence)
+  	@instagram = Instagram.user_recent_media(@deartrudence, {:count => 15})
   	# raise "hell"
   end
 end
