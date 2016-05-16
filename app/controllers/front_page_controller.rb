f2d57359142ee8868c62b888c5046ef344acb098
deartrudence <trudy.macnabb@gmail.com>
2016-05-16 16:27:22 -0400

    select blocks

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -2,8 +2,12 @@ class FrontPageController < ApplicationController
 	skip_before_action :authenticate_user!
   def show
   	@instagram = Instagram.user_recent_media(@deartrudence, {:count => 15})
-  	# raise "hell"
+  	@instwogram = Instagram.user_recent_media(@deartrudence, {:count => 15}).select { |pic| pic.caption.text.include?('#Repost')}
+  	@instwogram_count = Instagram.user_recent_media(@deartrudence).count { |pic| pic.caption.text.include?('#Repost')}
   	@tweets = $client.user_timeline[0..4]
+  	@jb = $client.search("from:WWFCanada", :result_type => "recent").take(50)
+  	@jb_count_with_hashtag = @jb.count {|tweet| tweet.text.include?('#ExploreTheSound') }
+  	@jb_with_hashtag = @jb.select { |tweet| tweet.text.include?('#ExploreTheSound') }
   	# raise "the roof"
   end
 end
