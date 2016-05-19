78fc0d80ff1081508afa1f52d2e027266bd92457
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-05-19 12:21:08 -0400

    updates to blog

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160518203106) do
+ActiveRecord::Schema.define(version: 20160519144754) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
