9fbde9e9d80e4a7a3c09051faa39d8e533e77636
deartrudence <trudy.macnabb@gmail.com>
2016-05-19 13:12:54 -0400

    commented out wp-api request on front page

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -48,11 +48,11 @@ class FrontPageController < ApplicationController
   	# header_string = header(params)
     # image_header_string = header(params)
   	#get those temporary token
-    response_request = make_request(method, uri, client_key, oauth_token, oauth_token_secret, client_secret)
+    # response_request = make_request(method, uri, client_key, oauth_token, oauth_token_secret, client_secret)
     # image_request = make_request(method, image_uri, client_key, oauth_token, oauth_token_secret, client_secret)
-  	response = request_data(response_request, uri, method)
+  	# response = request_data(response_request, uri, method)
     # image = request_data(image_request, image_uri, method)
-    @response = JSON.parse(response)
+    # @response = JSON.parse(response)
     # @image = JSON.parse(image)
 
   end
