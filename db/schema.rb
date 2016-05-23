0527be7fd0c95f7b652a9722a418d169da87fbe5
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-05-23 11:34:38 -0400

    sub-menu description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519144754) do
+ActiveRecord::Schema.define(version: 20160523153105) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 20160519144754) do
     t.integer  "main_menu_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "description"
   end
 
   add_index "sub_menus", ["main_menu_id"], name: "index_sub_menus_on_main_menu_id"
