9b53009a081d3d758b6a4de32e592bc09e17799d
deartrudence <trudy.macnabb@gmail.com>
2016-05-24 17:03:54 -0400

    stories styling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,11 @@ Rails.application.configure do
   config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.paths << Rails.root.join('/app/assets/fonts')
+  # Precompile additional assets
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
