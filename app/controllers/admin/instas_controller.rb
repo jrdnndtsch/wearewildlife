605f5779858c50ddd77b0c5257c0a13c1bc9abbf
deartrudence <trudy.macnabb@gmail.com>
2016-06-01 09:12:29 -0400

    change twitter hashtag

--- app/controllers/admin/instas_controller.rb
+++ app/controllers/admin/instas_controller.rb
@@ -3,7 +3,6 @@ class Admin::InstasController < ApplicationController
 	def upload	
 		@instas = Instagram.user_recent_media(@wwfcanada, {:count => 15})
 		@instas_with_hashtag = @instas.select { |pic| pic.caption.text.include?('#doubletigers') }
-		# raise "hell"
 		@instas_with_hashtag.last(5).each_with_index do |insta, index|
 			index_plus_one = index + 1
 			this_insta = Insta.where(id: index_plus_one).first_or_initialize({text: insta.caption.text, image_url: insta.images.standard_resolution.url})
