577cb292ab0213ba5b17ca50e8c434f96e2c93c1
deartrudence <trudy.macnabb@gmail.com>
2016-09-07 16:28:21 -0400

    make language dynamic in admin panel

--- app/models/concerns/language_preference.rb
+++ app/models/concerns/language_preference.rb
@@ -1,8 +1,24 @@
 module LanguagePreference
 
-
+	# Translation for Models
 	def header_lang(user)
-		user.preferred_language == 'en' ? 'Header' : 'Fr Header'
+		user.preferred_language == 'en' ? 'Header' : 'Entête'
+	end
+
+	def calls_to_action_lang(user)
+		user.preferred_language == 'en' ? 'Calls to Action' : 'Fr Calls to Action'
+	end
+
+	def generic_stories_lang(user)
+		user.preferred_language == 'en' ? 'Generic Stories' : 'Fr Generic Stories'
+	end
+
+	def menu_lang(user)
+		user.preferred_language == 'en' ? 'Menu' : 'Le Menu'
+	end
+
+	def featured_member_lang(user)
+		user.preferred_language == 'en' ? 'Featured Member' : 'Membre en Vedette'
 	end
 
 	def blog_lang(user)
@@ -13,16 +29,61 @@ module LanguagePreference
 		user.preferred_language == 'en' ? element.title : element.fr_title
 	end
 
+	# Generic Language translation
+
+	def admin_lang(user)
+		user.preferred_language == 'en' ? 'WWF Admin' : "WWF Panneau d'Administration"	
+	end
+
+	def logged_in_lang(user)
+		user.preferred_language == 'en' ? 'Logged in as' : 'Fr logged in as'	
+	end
+
+	def logout_lang(user)
+		user.preferred_language == 'en' ? 'Logout' : 'Fr logout'	
+	end
+
+	def recent_updates_lang(user)
+		user.preferred_language == 'en' ? 'Recent Updates' : 'Fr Recent Updates'	
+	end
+
+	def all_lang(user)
+		user.preferred_language == 'en' ? 'All' : 'Tous'
+	end
+	def title_lang(user)
+		user.preferred_language == 'en' ? 'Title' : 'Titre'
+	end
+
+	def featured_lang(user)
+		user.preferred_language == 'en' ? 'Featured' : 'Fr Featured'
+	end
+
+	def selected_lang(user)
+		user.preferred_language == 'en' ? 'Selected' : 'Choisi'
+	end
+
+	def approved_lang(user)
+		user.preferred_language == 'en' ? 'Approved?' : 'A Approuvé?'
+	end
+
+	def date_lang(user)
+		user.preferred_language == 'en' ? 'Date' : 'Date'
+	end
+
 	def editing_lang(user)
 		user.preferred_language == 'en' ? 'Editing' : 'Fr Editing'
 	end
 
+	def edit_lang(user)
+		user.preferred_language == 'en' ? 'Edit' : 'Modifier'
+	end
+
 	def number_lang(user)
-		user.preferred_language == 'en' ? 'Number' : 'Fr Number'
+		user.preferred_language == 'en' ? 'Number' : 'Nombre'
 	end
 
 	def update_lang(user)
-		user.preferred_language == 'en' ? 'Update' : 'Fr Update'
+		user.preferred_language == 'en' ? 'Update' : 'Réactualiser'
 	end
 
 	def new_lang(user)
@@ -38,15 +99,15 @@ module LanguagePreference
 	end
 
 	def back_lang(user)
-		user.preferred_language == 'en' ? 'Back' : 'Fr Back'	
+		user.preferred_language == 'en' ? 'Back' : 'Reculer'	
 	end
 
 	def delete_lang(user)
-		user.preferred_language == 'en' ? 'Delete' : 'Fr Delete'	
+		user.preferred_language == 'en' ? 'Delete' : 'Effacer'	
 	end
 
 	def remove_lang(user)
-		user.preferred_language == 'en' ? 'Remove' : 'Fr Remove'	
+		user.preferred_language == 'en' ? 'Remove' : 'éliminer'	
 	end
 
 end
--- app/models/fr_blog.rb
+++ app/models/fr_blog.rb
@@ -3,6 +3,8 @@ class FrBlog < ActiveRecord::Base
 		has_attached_file :photo
 	  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
+	  include LanguagePreference
+	  
 	  has_paper_trail
 
 	  scope :are_approved, -> { where(approved: true)}
