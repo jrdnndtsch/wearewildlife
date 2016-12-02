0a58969f6ebdb80e6d3c074f484b4df413c88dd7
deartrudence <trudy.macnabb@gmail.com>
2016-12-02 13:42:56 -0500

    added wildcard to config.asset_host for french subdomain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
-  config.asset_host = 'http://countfornature.wwf.ca/'
+  config.asset_host = 'http://*.wwf.ca/'
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
