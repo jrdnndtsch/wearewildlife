291d356908a79e7baea58b9630d7a5156b701c46
trudy macnabb <trudy.macnabb@gmail.com>
2017-10-05 06:35:55 -0400

    fixed updatedBy module to not break if a user has been deleted

--- app/models/concerns/updated_by.rb
+++ app/models/concerns/updated_by.rb
@@ -3,10 +3,12 @@ module UpdatedBy
 		update_list = []
 		Struct.new("Update", :user, :time)
 		model.versions.each do |update|
-			time = update.created_at
 			user_id = update.whodunnit
-			user = User.find(user_id)
-			update_list.push(Struct::Update.new(user, time))
+			if User.where(id: user_id).present?
+				time = update.created_at
+				user = User.find(user_id)
+				update_list.push(Struct::Update.new(user, time))
+			end
 		end
 		return update_list
 	end
